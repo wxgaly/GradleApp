79db9adc41ca75617dd8763077ba4864fcfd5ecf
wxgaly <nova_wangxuguang@126.com>
2018-11-01 00:44:03 +0800

    1. 上传单链表修改

--- src/main/java/link/LinkMain.java
+++ src/main/java/link/LinkMain.java
@@ -10,10 +10,60 @@ package link;
 public class LinkMain {
 
     public static void main(String[] args) {
-        testLink();
+        testOneWayLink();
+//        testBothWayLink();
     }
 
-    private static void testLink() {
+    private static void testOneWayLink() {
+        OneWayLinkedList<Integer> linkedList = new OneWayLinkedList<>();
+
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+
+        System.out.println("linkedList.getSize() : " + linkedList.getSize());
+        System.out.println("linkedList.getHead() : " + linkedList.getHead());
+        System.out.println("linkedList.getLast() : " + linkedList.getLast());
+        System.out.println("----------------------------------------------");
+
+//        linkedList.remove(2);
+//        System.out.println("linkedList.remove(2)");
+//        System.out.println("linkedList.getSize() : " + linkedList.getSize());
+//        System.out.println("linkedList.getHead() : " + linkedList.getHead());
+//        System.out.println("linkedList.getLast() : " + linkedList.getLast());
+//        System.out.println("----------------------------------------------");
+
+        linkedList.remove(5);
+        System.out.println("linkedList.remove(5)");
+        System.out.println("linkedList.getSize() : " + linkedList.getSize());
+        System.out.println("linkedList.getHead() : " + linkedList.getHead());
+        System.out.println("linkedList.getLast() : " + linkedList.getLast());
+        System.out.println("----------------------------------------------");
+
+        linkedList.remove(1);
+        System.out.println("linkedList.remove(1)");
+        System.out.println("linkedList.getSize() : " + linkedList.getSize());
+        System.out.println("linkedList.getHead() : " + linkedList.getHead());
+        System.out.println("linkedList.getLast() : " + linkedList.getLast());
+        System.out.println("----------------------------------------------");
+
+        linkedList.remove(4);
+        System.out.println("linkedList.remove(4)");
+        System.out.println("linkedList.getSize() : " + linkedList.getSize());
+        System.out.println("linkedList.getHead() : " + linkedList.getHead());
+        System.out.println("linkedList.getLast() : " + linkedList.getLast());
+        System.out.println("----------------------------------------------");
+
+        int size = linkedList.getSize();
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("linkedList.getValue() : " + linkedList.getValue(i));
+        }
+    }
+
+    private static void testBothWayLink() {
 
         BothWayLinkedList<Integer> linkedList = new BothWayLinkedList<>();
 
--- src/main/java/link/OneWayLinkedList.java
+++ src/main/java/link/OneWayLinkedList.java
@@ -34,4 +34,83 @@ public class OneWayLinkedList<T> {
         return true;
 
     }
+
+    public boolean remove(T value) {
+        if (value != null) {
+
+            if (this.head != null && value.equals(this.head.value)) {
+                this.head = this.head.nextNode;
+                --this.size;
+                return true;
+            }
+
+            Node<T> temp;
+            for (temp = this.head; temp != null; temp = temp.nextNode) {
+                Node<T> nextNode = temp.nextNode;
+
+                if (nextNode != null) {
+                    if (value.equals(nextNode.value)) {
+                        temp.nextNode = nextNode.nextNode;
+                        if (temp.nextNode == null) {
+                            this.last = temp;
+                        }
+                        --this.size;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public T getValue(int index) {
+
+        Node<T> temp = this.head;
+
+        for (int i = 0; i < index; i++) {
+            temp = temp.nextNode;
+        }
+
+        return temp.value;
+    }
+
+    /**
+     * get size.
+     *
+     * @return this size of list.
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * get value of head.
+     *
+     * @return value.
+     */
+    public T getHead() {
+        T value = null;
+
+        if (head != null) {
+            value = head.value;
+        }
+
+        return value;
+    }
+
+    /**
+     * get value of last.
+     *
+     * @return value.
+     */
+    public T getLast() {
+        T value = null;
+
+        if (last != null) {
+            value = last.value;
+        }
+
+        return value;
+    }
 }
