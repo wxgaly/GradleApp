dacf95916c1f9b626d1de4756ad84cb69552198b
wangxuguang <nova_wangxuguang@126.com>
2019-02-28 14:29:42 +0800

    1. 子序列之和整除K（优化速度）

--- src/main/java/question/SubarraySumsDivisibleByK.java
+++ src/main/java/question/SubarraySumsDivisibleByK.java
@@ -1,5 +1,8 @@
 package question;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * question.SubarraySumsDivisibleByK
  *
@@ -17,20 +20,24 @@ public class SubarraySumsDivisibleByK {
     }
 
     public static int subarraysDivByK(int[] A, int K) {
-        int result = 0;
-
-        int len = A.length;
-        for (int i = 0; i < len; i++) {
-            int sum = 0;
-            for (int j = i; j < len; j++) {
-                sum += A[j];
-                if ((sum % K) == 0) {
-                    result++;
-                }
-            }
+        int[] sum = new int[A.length + 1];
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap();
+        int len = sum.length;
+        for (int i = 1; i < len; i++) {
+            sum[i] = (sum[i - 1] + A[i - 1]) % K;
+            if (sum[i] < 0)
+                sum[i] += K;
+            if (sum[i] == 0)
+                count++;
+            if (map.containsKey(sum[i])) {
+                count += map.get(sum[i]);
+                map.put(sum[i], map.get(sum[i]) + 1);
+            } else
+                map.put(sum[i], 1);
         }
+        return count;
 
-        return result;
     }
 
 }
