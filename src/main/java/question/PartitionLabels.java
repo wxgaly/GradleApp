571ff3e6cbd799f8fc2a92eae07d85b8acab021f
wangxuguang <nova_wangxuguang@126.com>
2019-03-04 18:05:02 +0800

    1. +1 划分字母区间（增加题目描述）

--- src/main/java/question/PartitionLabels.java
+++ src/main/java/question/PartitionLabels.java
@@ -5,6 +5,20 @@ import java.util.*;
 /**
  * question.PartitionLabels
  *
+ * A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+ *
+ * Example 1:
+ * Input: S = "ababcbacadefegdehijhklij"
+ * Output: [9,7,8]
+ * Explanation:
+ * The partition is "ababcbaca", "defegde", "hijhklij".
+ * This is a partition so that each letter appears in at most one part.
+ * A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+ * Note:
+ *
+ * S will have length in range [1, 500].
+ * S will consist of lowercase letters ('a' to 'z') only.
+ *
  * @author Created by WXG on 2019/3/4 004 16:51.
  * @version V1.0
  */
