0ef5b67980ec3e340da7389ff86f7a0dc0a40ccd
wangxuguang <nova_wangxuguang@126.com>
2019-03-14 21:20:34 +0800

    1. +1 最小路径和1

--- src/main/java/question/medium/MinimumPathSum.java
+++ src/main/java/question/medium/MinimumPathSum.java
@@ -15,9 +15,9 @@ package question.medium;
  * <p>
  * Input:
  * [
- *  [1,3,1],
- *  [1,5,1],
- *  [4,2,1]
+ * [1,3,1],
+ * [1,5,1],
+ * [4,2,1]
  * ]
  * Output: 7
  * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
@@ -30,6 +30,21 @@ public class MinimumPathSum {
     }
 
     public static int minPathSum(int[][] grid) {
+
+        int rows = grid[0].length, columns = grid.length, minSum = 0;
+
+        int row = 0, col = 0;
+        int start = grid[0][0];
+
+        for (int i = 0; i < rows; i++) {
+            int sum = onePathSum(i, col, grid);
+        }
+
+
+    }
+
+    private static int onePathSum(int row, int col, int[][] grid) {
+
 
     }
 
