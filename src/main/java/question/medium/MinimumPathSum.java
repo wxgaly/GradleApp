a6edda680b5de335dddb835db1817083a4b7857e
wangxuguang <nova_wangxuguang@126.com>
2019-03-14 20:48:31 +0800

    1. +1 最小路径和

---
+++ src/main/java/question/medium/MinimumPathSum.java
@@ -0,0 +1,36 @@
+package question.medium;
+
+/**
+ * question.medium.MinimumPathSum
+ *
+ * @author Created by WXG on 2019/3/14 014 19:06.
+ * @version V1.0
+ * <p>
+ * Given a m x n grid filled with non-negative numbers,
+ * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * <p>
+ * Note: You can only move either down or right at any point in time.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [
+ *  [1,3,1],
+ *  [1,5,1],
+ *  [4,2,1]
+ * ]
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ */
+
+public class MinimumPathSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int minPathSum(int[][] grid) {
+
+    }
+
+}
