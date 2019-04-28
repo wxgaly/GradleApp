06615fd3b6d91cd60b9f532162a1b36289e25536
wangxuguang <nova_wangxuguang@126.com>
2019-04-28 17:53:26 +0800

    1. +1  买卖股票的最佳时机

---
+++ src/main/java/question/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,58 @@
+package question.easy;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2019-04-28 17:49.
+ * @version V1.0
+ * <p>
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * If you were only permitted to complete at most one transaction
+ * (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * <p>
+ * Note that you cannot sell a stock before you buy one.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * Example 2:
+ * <p>
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class BestTimeToBuyAndSellStock {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProfit(int[] prices) {
+
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int max = 0;
+        int now = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                now = prices[j] - prices[i];
+                if (now > max) {
+                    max = now;
+                }
+            }
+        }
+        if (max < 0) {
+            return 0;
+        } else {
+            return max;
+        }
+
+    }
+
+}
