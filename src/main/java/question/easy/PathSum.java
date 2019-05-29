36768c1dc89971e6eed6134ad8e158acbdda17b8
wangxuguang <nova_wangxuguang@126.com>
2019-05-29 14:10:11 +0800

    1. +1 路径总和

---
+++ src/main/java/question/easy/PathSum.java
@@ -0,0 +1,61 @@
+package question.easy;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2019-05-29 14:04.
+ * @version V1.0
+ *
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding
+ * up all the values along the path equals the given sum.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given the below binary tree and sum = 22,
+ *
+ *       5
+ *      / \
+ *     4   8
+ *    /   / \
+ *   11  13  4
+ *  /  \      \
+ * 7    2      1
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ *
+ */
+public class PathSum {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 理解叶子节点的概念，到叶子节点才能算结束
+     *
+     * @param root
+     * @param sum
+     * @return
+     */
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        } else if (root.left == null && root.right == null) {
+            return sum == root.val;
+        } else {
+            return hasPathSum(root.left, sum - root.val) && hasPathSum(root.right, sum - root.val);
+        }
+    }
+
+}
