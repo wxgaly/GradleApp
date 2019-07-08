379dc5fed14ee77948b8e2b0578ed036a88aa5a5
wangxuguang <nova_wangxuguang@126.com>
2019-07-08 14:31:59 +0800

    1. +1 字符串中的第一个唯一字符

---
+++ src/main/java/question/easy/FirstUniqueCharacterInAString.java
@@ -0,0 +1,54 @@
+package question.easy;
+
+import java.util.HashMap;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2019-07-08 14:12.
+ * @version V1.0
+ *
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ *
+ * Examples:
+ *
+ * s = "leetcode"
+ * return 0.
+ *
+ * s = "loveleetcode",
+ * return 2.
+ * Note: You may assume the string contain only lowercase letters.
+ *
+ *
+ *
+ */
+public class FirstUniqueCharacterInAString {
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+    }
+
+    public static int firstUniqChar(String s) {
+        if (s == null || s.isEmpty()) {
+            return -1;
+        }
+
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+
+    }
+
+}
