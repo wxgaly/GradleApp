572041d3bffbc42208eb77a914c3dc5732589d45
wangxuguang <nova_wangxuguang@126.com>
2019-10-08 17:36:49 +0800

    1. +1  三个有序数组的交集，增加新解法

--- src/main/java/question/easy/IntersectionOfThreeSortedArrays.java
+++ src/main/java/question/easy/IntersectionOfThreeSortedArrays.java
@@ -1,6 +1,8 @@
 package question.easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -41,7 +43,7 @@ public class IntersectionOfThreeSortedArrays {
         int[] arr3 = new int[]{
                 1, 3, 4, 5, 8
         };
-        ArrayList arrayList = (ArrayList) intersectionOfThreeSortedArrays.arraysIntersection1(arr1, arr2, arr3);
+        ArrayList arrayList = (ArrayList) intersectionOfThreeSortedArrays.arraysIntersection2(arr1, arr2, arr3);
         System.out.println(arrayList.toString());
     }
 
@@ -81,5 +83,55 @@ public class IntersectionOfThreeSortedArrays {
 
         return list1;
     }
+
+    /**
+     * 折半查找虽然速度较快，但是依然需要多次循环
+     *
+     * @param arr1
+     * @param arr2
+     * @param arr3
+     * @return
+     */
+    public List<Integer> arraysIntersection2(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : arr1) {
+            if (Arrays.binarySearch(arr2, i) >= 0 && Arrays.binarySearch(arr3, i) >= 0) {
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * 标记法，一次循环。
+     *
+     * @param arr1
+     * @param arr2
+     * @param arr3
+     * @return
+     */
+    public List<Integer> arraysIntersection4(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> nums = new LinkedList<>();
+        for(int i=0,j=0,k=0;i<arr1.length&&j<arr2.length&&k<arr3.length;i++,j++,k++){
+            int a = arr1[i];
+            int b = arr2[j];
+            int c = arr3[k];
+            if(a==b&&b==c){
+                nums.add(a);
+            }
+            if(a>b||a>c){
+                i--;
+            }
+            if(b>a||b>c){
+                j--;
+            }
+            if(c>a||c>b){
+                k--;
+            }
+        }
+        return nums;
+    }
+
 
 }
