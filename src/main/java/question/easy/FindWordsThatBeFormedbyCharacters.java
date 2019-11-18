e3ae8b33d5b37b2e7dac80ffa8f5d56d4ce01a83
wangxuguang <nova_wangxuguang@126.com>
2019-11-18 17:39:47 +0800

    1. +1 拼写单词

---
+++ src/main/java/question/easy/FindWordsThatBeFormedbyCharacters.java
@@ -0,0 +1,110 @@
+package question.easy;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2019-11-18 17:23
+ * @version V1.0
+ *
+ * You are given an array of strings words and a string chars.
+ *
+ * A string is good if it can be formed by characters from chars (each character can only be used once).
+ *
+ * Return the sum of lengths of all good strings in words.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: words = ["cat","bt","hat","tree"], chars = "atach"
+ * Output: 6
+ * Explanation:
+ * The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+ * Example 2:
+ *
+ * Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+ * Output: 10
+ * Explanation:
+ * The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ *  
+ *
+ * Note:
+ *
+ * 1 <= words.length <= 1000
+ * 1 <= words[i].length, chars.length <= 100
+ * All strings contain lowercase English letters only.
+ *
+ *
+ */
+public class FindWordsThatBeFormedbyCharacters {
+
+    public static void main(String[] args) {
+        String word = "abcd";
+        System.out.println(word.substring(0, 2));
+    }
+
+    public int countCharacters(String[] words, String chars) {
+        int count = 0;
+
+        for (String word : words) {
+            char[] wordChars = word.toCharArray();
+            String temp = new String(chars.getBytes());
+            for (char wordChar : wordChars) {
+                int len = temp.length();
+                int index = 0;
+                boolean isFind = false;
+                for (; index < len; index++) {
+                    if (temp.charAt(index) == wordChar) {
+                        isFind = true;
+                        if (index == 0) {
+                            temp = temp.substring(index + 1);
+                        } else if(index == len - 1) {
+                            temp = temp.substring(0, index);
+                        } else {
+                            temp = temp.substring(0, index) + temp.substring(index, len);
+                        }
+
+                    }
+                }
+                //有一个char没有找到，直接找下一个单词
+                if (!isFind) {
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * 1. 用字典记录所有字符出现次数
+     * 2. 遍历words，
+     * 3. 遍历word找到char与之匹配的char，然后次数减一，并且
+     * @param words
+     * @param chars
+     * @return
+     */
+    public int countCharacters1(String[] words, String chars) {
+        char[] target = chars.toCharArray();
+        int[] temp = new int[26];
+        int result = 0;
+        for (char c : target) {
+            temp[c - 'a']++;
+        }
+        for (String word : words) {
+            char[] src = word.toCharArray();
+            int one = 0;
+            int[] t = temp.clone();
+            for (char c : src) {
+                if(t[c - 'a']-- > 0){
+                    one++;
+                }
+            }
+            if (one == word.length()){
+                result += one;
+            }
+        }
+        return result;
+    }
+
+}
--- src/main/java/question/easy/RemoveAdjacentDuplicatesInString.java
+++ src/main/java/question/easy/RemoveAdjacentDuplicatesInString.java
@@ -7,29 +7,28 @@ import java.util.ArrayList;
  *
  * @author Created by WXG on 2019-11-14 14:29
  * @version V1.0
- *
+ * <p>
  * Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
- *
+ * <p>
  * We repeatedly make duplicate removals on S until we no longer can.
- *
+ * <p>
  * Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
- *
+ * <p>
  *  
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: "abbaca"
  * Output: "ca"
  * Explanation:
  * For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move. 
  * The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
  *  
- *
+ * <p>
  * Note:
- *
+ * <p>
  * 1 <= S.length <= 20000
  * S consists only of English lowercase letters.
- *
  */
 public class RemoveAdjacentDuplicatesInString {
 
