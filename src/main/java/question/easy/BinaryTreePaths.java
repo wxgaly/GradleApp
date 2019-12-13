511c9d28fee0c199072ef2afe1801f7991fa34c9
wangxuguang <nova_wangxuguang@126.com>
2019-12-13 14:37:17 +0800

    1. +1 二叉树的所有路径

---
+++ src/main/java/question/easy/BinaryTreePaths.java
@@ -0,0 +1,68 @@
+package question.easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2019-12-13 14:33
+ * @version V1.0
+ *
+ * Given a binary tree, return all root-to-leaf paths.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Input:
+ *
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ *
+ * Output: ["1->2->5", "1->3"]
+ *
+ * Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+ *
+ *
+ */
+public class BinaryTreePaths {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public void construct_paths(TreeNode root, String path, LinkedList<String> paths) {
+        if (root != null) {
+            path += Integer.toString(root.val);
+            if ((root.left == null) && (root.right == null))  // 当前节点是叶子节点
+                paths.add(path);  // 把路径加入到答案中
+            else {
+                path += "->";  // 当前节点不是叶子节点，继续递归遍历
+                construct_paths(root.left, path, paths);
+                construct_paths(root.right, path, paths);
+            }
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        LinkedList<String> paths = new LinkedList();
+        construct_paths(root, "", paths);
+        return paths;
+    }
+
+
+}
