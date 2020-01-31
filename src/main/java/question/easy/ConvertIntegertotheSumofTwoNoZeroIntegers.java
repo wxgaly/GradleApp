14dc8e579915fbb188fdf7d52c82fba4e2f2edc3
wangxuguang <nova_wangxuguang@126.com>
2020-01-31 15:10:08 +0800

    1. +1 将整数转换为两个无零的整数的和

---
+++ src/main/java/question/easy/ConvertIntegertotheSumofTwoNoZeroIntegers.java
@@ -0,0 +1,70 @@
+package question.easy;
+
+import java.util.Arrays;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2020-01-31 15:04
+ * @version V1.0
+ *
+ * Given an integer n. No-Zero integer is a positive integer which doesn't contain any 0 in its decimal representation.
+ *
+ * Return a list of two integers [A, B] where:
+ *
+ * A and B are No-Zero integers.
+ * A + B = n
+ * It's guarateed that there is at least one valid solution. If there are many valid solutions you can return any of them.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: [1,1]
+ * Explanation: A = 1, B = 1. A + B = n and both A and B don't contain any 0 in their decimal representation.
+ * Example 2:
+ *
+ * Input: n = 11
+ * Output: [2,9]
+ * Example 3:
+ *
+ * Input: n = 10000
+ * Output: [1,9999]
+ * Example 4:
+ *
+ * Input: n = 69
+ * Output: [1,68]
+ * Example 5:
+ *
+ * Input: n = 1010
+ * Output: [11,999]
+ *  
+ *
+ * Constraints:
+ *
+ * 2 <= n <= 10^4
+ *
+ *
+ */
+public class ConvertIntegertotheSumofTwoNoZeroIntegers {
+
+    public static void main(String[] args) {
+        ConvertIntegertotheSumofTwoNoZeroIntegers convertIntegertotheSumofTwoNoZeroIntegers = new ConvertIntegertotheSumofTwoNoZeroIntegers();
+        System.out.println(Arrays.toString(convertIntegertotheSumofTwoNoZeroIntegers.getNoZeroIntegers(1010)));
+    }
+
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++) {
+            int j = n - i;
+            if (!(i + "" + j).contains("0")) {
+                return new int[]{
+                        i, j
+                };
+            }
+        }
+
+        return new int[0];
+    }
+
+}
