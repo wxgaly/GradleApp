53eabd1b513f702fe1ce7d799def184bbfeb6fbf
wangxuguang <nova_wangxuguang@126.com>
2020-02-02 15:09:33 +0800

    1. +1 移动零(增加新解)

--- src/main/java/question/easy/MoveZeroes.java
+++ src/main/java/question/easy/MoveZeroes.java
@@ -83,4 +83,17 @@ public class MoveZeroes {
         nums[b] = temp;
     }
 
+    public void moveZeroes1(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (; index < nums.length; index++) {
+            nums[index] = 0;
+        }
+    }
+
 }
