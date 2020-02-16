d5c65add32e3501f738f154468008aa03e26fd6e
wangxuguang <nova_wangxuguang@126.com>
2020-02-16 18:01:26 +0800

    1. +1 复写零

---
+++ src/main/java/question/easy/DuplicateZeros.java
@@ -0,0 +1,87 @@
+package question.easy;
+
+import java.util.Arrays;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2020-02-16 17:45
+ * @version V1.0
+ *
+ * Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+ *
+ * Note that elements beyond the length of the original array are not written.
+ *
+ * Do the above modifications to the input array in place, do not return anything from your function.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: [1,0,2,3,0,4,5,0]
+ * Output: null
+ * Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+ * Example 2:
+ *
+ * Input: [1,2,3]
+ * Output: null
+ * Explanation: After calling your function, the input array is modified to: [1,2,3]
+ *  
+ *
+ * Note:
+ *
+ * 1 <= arr.length <= 10000
+ * 0 <= arr[i] <= 9
+ *
+ */
+public class DuplicateZeros {
+
+    public static void main(String[] args) {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
+        duplicateZeros.duplicateZeros(new int[]{
+                1, 0, 2, 3, 0, 4, 5, 0
+        });
+    }
+
+    public void duplicateZeros(int[] arr) {
+        int possibleDups = 0;
+        int length_ = arr.length - 1;
+
+        // Find the number of zeros to be duplicated
+        // Stopping when left points beyond the last element in the original array
+        // which would be part of the modified array
+        for (int left = 0; left <= length_ - possibleDups; left++) {
+
+            // Count the zeros
+            if (arr[left] == 0) {
+
+                // Edge case: This zero can't be duplicated. We have no more space,
+                // as left is pointing to the last element which could be included
+                if (left == length_ - possibleDups) {
+                    // For this zero we just copy it without duplication.
+                    arr[length_] = 0;
+                    length_ -= 1;
+                    break;
+                }
+                possibleDups++;
+            }
+        }
+
+        // Start backwards from the last element which would be part of new array.
+        int last = length_ - possibleDups;
+
+        // Copy zero twice, and non zero once.
+        for (int i = last; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i + possibleDups] = 0;
+                possibleDups--;
+                arr[i + possibleDups] = 0;
+            } else {
+                arr[i + possibleDups] = arr[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
