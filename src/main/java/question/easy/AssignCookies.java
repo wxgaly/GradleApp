d07756e18a0160733bbf0fc320631677371f4473
wangxuguang <nova_wangxuguang@126.com>
2020-03-10 15:57:10 +0800

    1. +1 分发饼干

---
+++ src/main/java/question/easy/AssignCookies.java
@@ -0,0 +1,82 @@
+package question.easy;
+
+import java.util.Arrays;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2020-03-10 15:45
+ * @version V1.0
+ *
+ * Assume you are an awesome parent and want to give your children some cookies. But,
+ * you should give each child at most one cookie. Each child i has a greed factor gi,
+ * which is the minimum size of a cookie that the child will be content with;
+ * and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i,
+ * and the child i will be content.
+ * Your goal is to maximize the number of your content children and output the maximum number.
+ *
+ * Note:
+ * You may assume the greed factor is always positive.
+ * You cannot assign more than one cookie to one child.
+ *
+ * Example 1:
+ * Input: [1,2,3], [1,1]
+ *
+ * Output: 1
+ *
+ * Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
+ * And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+ * You need to output 1.
+ * Example 2:
+ * Input: [1,2], [1,2,3]
+ *
+ * Output: 2
+ *
+ * Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
+ * You have 3 cookies and their sizes are big enough to gratify all of the children,
+ * You need to output 2.
+ *
+ *
+ */
+public class AssignCookies {
+
+    public static void main(String[] args) {
+        AssignCookies assignCookies = new AssignCookies();
+        System.out.println(assignCookies.findContentChildren(new int[]{10, 9, 8, 7}, new int[]{5, 6, 7, 8}));
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int index = 0;
+        for (int i = 0; i < g.length; i++) {
+            for (; index < s.length; ) {
+                if (s[index] >= g[i]) {
+                    count++;
+                    index++;
+                    break;
+                }
+                index++;
+            }
+        }
+
+        return count;
+    }
+
+    public int findContentChildren1(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i;
+    }
+
+}
