8601feb8720b4cf703aa4b6ffae13ba17d7e1dc9
wangxuguang <nova_wangxuguang@126.com>
2020-03-31 14:08:53 +0800

    1. +1 最大距离

---
+++ src/main/java/question/easy/MaximizeDistancetoClosestPerson.java
@@ -0,0 +1,69 @@
+package question.easy;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2020-03-31 13:50
+ * @version V1.0
+ *
+ * In a row of seats, 1 represents a person sitting in that seat, and 0 represents that the seat is empty. 
+ *
+ * There is at least one empty seat, and at least one person sitting.
+ *
+ * Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized. 
+ *
+ * Return that maximum distance to closest person.
+ *
+ * Example 1:
+ *
+ * Input: [1,0,0,0,1,0,1]
+ * Output: 2
+ * Explanation:
+ * If Alex sits in the second open seat (seats[2]), then the closest person has distance 2.
+ * If Alex sits in any other open seat, the closest person has distance 1.
+ * Thus, the maximum distance to the closest person is 2.
+ * Example 2:
+ *
+ * Input: [1,0,0,0]
+ * Output: 3
+ * Explanation:
+ * If Alex sits in the last seat, the closest person is 3 seats away.
+ * This is the maximum distance possible, so the answer is 3.
+ * Note:
+ *
+ * 1 <= seats.length <= 20000
+ * seats contains only 0s or 1s, at least one 0, and at least one 1.
+ *
+ *
+ */
+public class MaximizeDistancetoClosestPerson {
+
+    public static void main(String[] args) {
+        MaximizeDistancetoClosestPerson closestPerson = new MaximizeDistancetoClosestPerson();
+        System.out.println(closestPerson.maxDistToClosest(new int[]{
+                1, 0, 0, 0, 1, 0, 1
+        }));
+    }
+
+    public int maxDistToClosest(int[] seats) {
+        int length = seats.length;
+        int end = 0;
+        int max = -1;
+        for (int i = 0; i < length; i++) {
+            if (seats[i] == 1 && i != length - 1) {
+                end = i + 1;
+                while (seats[end] != 1) {
+                    end++;
+                }
+                int temp = end - i;
+                if (temp > max) {
+                    max = temp;
+                }
+                i = end - 1;
+            }
+        }
+
+        return max - 1;
+    }
+
+}
