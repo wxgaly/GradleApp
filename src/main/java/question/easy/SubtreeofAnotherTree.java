75a4d494f61e98539b30f9d8b1784a78850c4008
wangxuguang <nova_wangxuguang@126.com>
2020-05-13 14:04:41 +0800

    1. +1 另一个树的子树

---
+++ src/main/java/question/easy/SubtreeofAnotherTree.java
@@ -0,0 +1,95 @@
+package question.easy;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2020-05-13 13:53
+ * @version V1.0
+ *
+ * Given two non-empty binary trees s and t,
+ * check whether tree t has exactly the same structure and node values with a subtree of s.
+ * A subtree of s is a tree consists of a node in s and all of this node's descendants.
+ * The tree s could also be considered as a subtree of itself.
+ *
+ * Example 1:
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ * Return true, because t has the same structure and node values with a subtree of s.
+ *  
+ *
+ * Example 2:
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ * Return false.
+ *
+ * 这绝对不是一道随随便便的简单题
+ *
+ */
+public class SubtreeofAnotherTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * DFS暴力匹配
+     * 官方题解： https://leetcode-cn.com/problems/subtree-of-another-tree/solution/ling-yi-ge-shu-de-zi-shu-by-leetcode-solution/
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+
+        return check(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean check(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+
+        if (s == null || t == null) {
+            return false;
+        }
+
+        if (s.val == t.val) {
+            return check(s.left, t.left) && check(s.right, t.right);
+        }
+
+        return false;
+    }
+}
