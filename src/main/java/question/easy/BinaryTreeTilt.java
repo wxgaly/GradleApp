4c3c53a40a3511e01f5dad06e9e7e02a4a67350b
wangxuguang <nova_wangxuguang@126.com>
2020-05-25 13:48:48 +0800

    1. +1 二叉树的坡度

---
+++ src/main/java/question/easy/BinaryTreeTilt.java
@@ -0,0 +1,83 @@
+package question.easy;
+
+/**
+ * question.easy.
+ *
+ * @author Created by WXG on 2020-05-25 13:39
+ * @version V1.0
+ *
+ * Given a binary tree, return the tilt of the whole tree.
+ *
+ * The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values
+ * and the sum of all right subtree node values. Null node has tilt 0.
+ *
+ * The tilt of the whole tree is defined as the sum of all nodes' tilt.
+ *
+ * Example:
+ * Input:
+ *          1
+ *        /   \
+ *       2     3
+ * Output: 1
+ * Explanation:
+ * Tilt of node 2 : 0
+ * Tilt of node 3 : 0
+ * Tilt of node 1 : |2-3| = 1
+ * Tilt of binary tree : 0 + 0 + 1 = 1
+ * Note:
+ *
+ * The sum of node values in any subtree won't exceed the range of 32-bit integer.
+ * All the tilt values won't exceed the range of 32-bit integer.
+ *
+ *
+ */
+public class BinaryTreeTilt {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int res = 0;
+
+    public int findTilt(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+
+        int left = findTilt(root.left);
+        int right = findTilt(root.right);
+
+        return root.val + Math.abs(left - right);
+    }
+
+    int tilt = 0;
+
+    public int findTilt1(TreeNode root) {
+        traverse(root);
+        return tilt;
+    }
+
+    public int traverse(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = traverse(root.left);
+        int right = traverse(root.right);
+        tilt += Math.abs(left - right);
+        return left + right + root.val;
+    }
+
+}
